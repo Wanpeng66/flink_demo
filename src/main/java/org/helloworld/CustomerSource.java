165d2aea6ba27b4b2c52c89b932df1ac5839d787
Peng Wan(LNG-SHA) <peng.wan@lexisnexis.com>
2022-11-23 15:00:07 +0800

    first-commit

--- src/main/java/org/helloworld/CustomerSource.java
+++ src/main/java/org/helloworld/CustomerSource.java
@@ -2,6 +2,12 @@ package org.helloworld;
 
 import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.RichFlatMapFunction;
+import org.apache.flink.api.common.state.AggregatingState;
+import org.apache.flink.api.common.state.AggregatingStateDescriptor;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.Configuration;
@@ -30,7 +36,53 @@ public class CustomerSource {
                 collector.collect(Tuple2.of(s, 1L));
             }
         }).keyBy((KeySelector<Tuple2<String, Long>, String>) stringLongTuple2 -> stringLongTuple2.f0)
-                .window(SlidingEventTimeWindows.of(Time.seconds(5), Time.seconds(2)))
+                .flatMap(new RichFlatMapFunction<Tuple2<String, Long>, String>() {
+                    ValueState<Long> num;
+                    AggregatingState<Tuple2<String, Long>, Long> aggregatingState;
+
+                    @Override
+                    public void open(Configuration parameters) throws Exception {
+                        aggregatingState = getRuntimeContext().getAggregatingState(new AggregatingStateDescriptor<Tuple2<String, Long>, Tuple2<Long, Long>, Long>("agg", new AggregateFunction<Tuple2<String, Long>, Tuple2<Long, Long>, Long>() {
+                            @Override
+                            public Tuple2<Long, Long> createAccumulator() {
+                                return Tuple2.of(0L, 0L);
+                            }
+
+                            @Override
+                            public Tuple2<Long, Long> add(Tuple2<String, Long> v1, Tuple2<Long, Long> accumulator) {
+                                return Tuple2.of(v1.f1+accumulator.f0, accumulator.f1+1L);
+                            }
+
+                            @Override
+                            public Long getResult(Tuple2<Long, Long> accumulator) {
+                                return accumulator.f0 / accumulator.f1;
+                            }
+
+                            @Override
+                            public Tuple2<Long, Long> merge(Tuple2<Long, Long> longLongTuple2, Tuple2<Long, Long> acc1) {
+                                return null;
+                            }
+                        }, Types.TUPLE(Types.LONG, Types.LONG)));
+                        num = getRuntimeContext().getState(new ValueStateDescriptor<Long>("num", Long.class));
+                        super.open(parameters);
+                    }
+
+                    @Override
+                    public void flatMap(Tuple2<String, Long> stringLongTuple2, Collector<String> collector) throws Exception {
+                        Long value = num.value();
+                        if (null==value) {
+                            value = 1l;
+                        } else if (value<5) {
+                            value++;
+                        }else {
+                            value = 0l;
+                            collector.collect(stringLongTuple2.f0+"出现了5次");
+                            num.clear();
+                        }
+                        num.update(value);
+                    }
+                })
+                //.window(SlidingEventTimeWindows.of(Time.seconds(5), Time.seconds(2)))
                 /*.aggregate(new AggregateFunction<Tuple2<String, Long>, Map<String, Integer>, Map<String, Integer>>() {
                     @Override
                     public Map<String, Integer> createAccumulator() {
@@ -54,13 +106,14 @@ public class CustomerSource {
                         return null;
                     }
                 }).print();*/
-                .sum(1).print();
+                //.sum(1)
+                .print();
         environment.execute("timeWindow");
     }
 
     private static class StringSourceFunction implements SourceFunction<String> {
         volatile boolean flag = false;
-        String[] array = new String[]{"java", "python", "golang", "c++", "scala", "kotlin", "erlang"};
+        String[] array = new String[]{"java", "python", "golang", "c++", "scala", "kotlin", "erlang", "php", "c#"};
         AtomicInteger index = new AtomicInteger(0);
 
         @Override
